70a227195d8c8e341c5fc467ecd2cb16b13f833d
Aditya Oberai <adityaoberai1@gmail.com>
2021-07-03 11:32:02 +0530

    Add project files.

---
+++ TodoAPI/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Time { get; set; }
+
+        public Todo ConvertDTO(TodoDTO todoDTO)
+        {
+            return new Todo { Name = todoDTO.Name, Description = todoDTO.Description, Time = todoDTO.Time };
+        }
+    }
+}
